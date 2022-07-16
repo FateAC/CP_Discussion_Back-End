92dcd53d43b401ecba556848207db8176ce0d002
LoveSnowEx <lsktw22@gmail.com>
2022-07-16 15:45:28 +0800

    fix(auth): move parseToken to authDirective

--- directive/authDirective.go
+++ directive/authDirective.go
@@ -4,32 +4,52 @@ import (
 	"CP_Discussion/auth"
 	"CP_Discussion/database"
 	"context"
+	"errors"
 
 	"github.com/99designs/gqlgen/graphql"
 	"github.com/vektah/gqlparser/v2/gqlerror"
 )
 
+func parseContextClaims(ctx context.Context) (*auth.Claims, error) {
+	token, ok := ctx.Value(string("token")).(string)
+	if !ok || token == "" {
+		return nil, errors.New("no token provided")
+	}
+	bearer := "Bearer "
+	token = token[len(bearer):]
+	claims, err := auth.ParseToken(token)
+	if err != nil {
+		return nil, err
+	}
+	return claims, nil
+}
+
 func AuthDirective(ctx context.Context, _ interface{}, next graphql.Resolver) (interface{}, error) {
-	claims, ok := ctx.Value(string("auth")).(*auth.Claims)
-	if claims == nil || !ok {
+	_, err := parseContextClaims(ctx)
+	if err != nil {
 		return nil, &gqlerror.Error{
-			Message: "Access Denied",
+			Message: "Access Denied: " + err.Error(),
 		}
 	}
 	return next(ctx)
 }
 
 func AdminDirective(ctx context.Context, _ interface{}, next graphql.Resolver) (interface{}, error) {
-	claims, ok := ctx.Value(string("auth")).(*auth.Claims)
-	if claims == nil || !ok {
+	claims, err := parseContextClaims(ctx)
+	if err != nil {
 		return nil, &gqlerror.Error{
-			Message: "Access Denied",
+			Message: "Access Denied: " + err.Error(),
 		}
 	}
 	member, err := database.DBConnect.FindMemberById(claims.UserID)
-	if err != nil || !member.IsAdmin {
+	if err != nil {
 		return nil, &gqlerror.Error{
-			Message: "Access Denied",
+			Message: "Access Denied: member not found",
+		}
+	}
+	if member.IsAdmin {
+		return nil, &gqlerror.Error{
+			Message: "Access Denied: member is not a admin",
 		}
 	}
 	return next(ctx)
--- middleware/authMiddleware.go
+++ middleware/authMiddleware.go
@@ -1,7 +1,6 @@
 package middleware
 
 import (
-	"CP_Discussion/auth"
 	"context"
 	"net/http"
 
@@ -10,6 +9,7 @@ import (
 
 func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		c.Status(http.StatusOK)
 		w, r := c.Writer, c.Request
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
@@ -17,20 +17,7 @@ func AuthMiddleware() gin.HandlerFunc {
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 
 		token := r.Header.Get("Authorization")
-		if token == "" {
-			c.Status(http.StatusOK)
-			c.Next()
-			return
-		}
-		bearer := "Bearer "
-		token = token[len(bearer):]
-
-		claims, err := auth.ParseToken(token)
-		if err != nil {
-			c.Status(http.StatusForbidden)
-			return
-		}
-		ctx := context.WithValue(r.Context(), string("auth"), claims)
+		ctx := context.WithValue(r.Context(), string("token"), token)
 
 		c.Request = r.WithContext(ctx)
 		c.Next()
